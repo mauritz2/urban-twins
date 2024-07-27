9be581852e1206c97d4ff04565a7bd1ca89ed31b
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2024-07-27 10:35:14 +0200

    Search suggestions added

--- urban-twins/src/components/SearchBox.js
+++ urban-twins/src/components/SearchBox.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const cities = [
+    "New York",
+    "Newark",
+    "Stockholm",
+    "Paris",
+    "Nice",
+    "Sarajevo",
+    "Prague"
+]
 
 const SearchBox = ({ placeholder, label}) => {
-    const handleSearch = (event) => {
-        // Handle search logic here
+    const [inputValue, setInputValue] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+
+    const handleChange = (event) => {
+        const value = event.target.value;
+        setInputValue(value);
+
+        if (value.length > 0) {
+            const filteredSuggestions = cities.filter(city =>city.toLowerCase().startsWith(value.toLowerCase())
+            );   
+            setSuggestions(filteredSuggestions);
+        } else {
+            setSuggestions([]);
+        }
+    };
+
+    const handleSuggestionClick = (suggestion) => {
+        setInputValue(suggestion);
+        setSuggestions([]);
     };
 
     return (
         <div>
             <label htmlFor="search-box">{label}</label>
-            <input id="search-box" type="text" placeholder={placeholder} onChange={handleSearch} />
-            {/* Add any additional elements or logic here */}
+            <input
+                type="text"
+                placeholder={placeholder}
+                value={inputValue}
+                onChange={handleChange}
+            />
+           {suggestions.length > 0 && (
+            <ul className="suggestions-list">
+                {suggestions.map((suggestion, index) => (
+                    <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                        {suggestion}
+                    </li>
+                ))}
+            </ul>
+           )}
         </div>
     );
 };
