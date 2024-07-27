c4e9d79268529554a45194b3a558e9322f0442b6
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2024-07-27 08:30:15 +0200

    Contrast layer and layout updates

--- urban-twins/src/App.js
+++ urban-twins/src/App.js
@@ -8,14 +8,17 @@ function App() {
           <h1>Urban Twins</h1>
         </div>
         <div className="detailed-text">
-          <p>Understand the neighborhoods in a new city by finding equivalent neighborhoods in your own city</p>
+          <p>
+            Use AI to understand the vibe of neighborhoods of a new city by finding their equivalent in a city you know. 
+            For example when planning a vacation, booking a hotel or moving to a new city
+          </p>
         </div>
         <div className="search-boxes">
           <div>
             <SearchBox placeholder="New York" label="City to explore" />
           </div>
           <div>
-            <SearchBox placeholder="Paris" label="City to compare" />
+            <SearchBox placeholder="Paris" label="City you know" />
           </div>
         </div>
       </div>
