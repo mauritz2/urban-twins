7f9a8484a05ab4936664cbdbc07bd03c33418f39
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2024-08-11 14:35:52 +0200

    Dynamic labels implemented

--- urban-twins/src/components/MapComponent.js
+++ urban-twins/src/components/MapComponent.js
@@ -4,13 +4,13 @@ import '../Map.css';
 function MapComponent() {
   
   const mapRef = useRef(null);
-  const centerOfManhattan = { lat: 40.7831, lng: -73.9712 };
+  const nycCenter = { lat: 40.7831, lng: -73.9712 };
   const nycBoundaries = {"north": 40.915255, "south": 40.559000, "west": -74.255735, "east": -73.700272};
 
     useEffect(() => {
       const initMap = () => {
         const map = new window.google.maps.Map(mapRef.current, {
-          center: centerOfManhattan,
+          center: nycCenter,
           zoom: 12,
           strictBounds: true,
           restriction: {
@@ -23,30 +23,29 @@ function MapComponent() {
           },
         });
 
-        const neighborhoodsToDisplay = ['east-village.txt', 'upper-west-side.txt'];
+        //const neighborhoodsToDisplay = {'east-village.txt', 'upper-west-side.txt'];
+        const neighborhoodsToDisplay = [
+          {filename: "east-village.txt", label: "East Village (Marais)"},
+          {filename: "upper-west-side.txt", label: "Upper West Side (7th arr.)"},
+        ]
 
-        neighborhoodsToDisplay.forEach(neighborhoodFile => {
-
-          fetch('./neighborhoods/' + neighborhoodFile)
+        neighborhoodsToDisplay.forEach(neighborhood => {
+          fetch('./neighborhoods/' + neighborhood.filename)
             .then(response => response.text())
             .then(data => {
               let neighborhoodCoords = parseCoords(data);
               let neighborhoodMapPolygon = getMapPolygon(neighborhoodCoords);
               neighborhoodMapPolygon.setMap(map);
 
-              // Set the label of the polygon
-              const bounds = new window.google.maps.LatLngBounds();
-              neighborhoodCoords.forEach(coord => bounds.extend(coord));
-              const center = bounds.getCenter();
+              let center = getNeighborhoodCenter(neighborhoodCoords);
               const mapLabel = new window.MapLabel({
-                text: "East Village (Marais)",
+                text: neighborhood.label,
                 position: center,
                 map: map,
-                fontSize: 12,
+                fontSize: 14,
                 align: 'center',
               });
-
-          mapLabel.set('position', center);
+             mapLabel.set('position', center);
         })
       });
     };
@@ -61,7 +60,7 @@ function MapComponent() {
       </div>
     );
   }
-  
+
 const parseCoords = (coordsText) => {
   return coordsText.trim().split('\n').map(line => {
     const [lng, lat] = line.trim().split(/\s+/);
@@ -78,6 +77,12 @@ const getMapPolygon = (neighborhoodCoords) => {
     fillColor: "#FF0000",
     fillOpacity: 0.35,
   });
+}
+
+const getNeighborhoodCenter = (neighborhoodCoords) => {
+  const bounds = new window.google.maps.LatLngBounds();
+  neighborhoodCoords.forEach(coord => bounds.extend(coord));
+  return bounds.getCenter();
 }
 
 export default MapComponent;
