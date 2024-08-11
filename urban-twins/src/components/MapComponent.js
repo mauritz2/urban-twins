bd3984db980cc64eb673ed9ab963d17904be8134
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2024-08-11 13:58:23 +0200

    loop to display multiple neighborhoods

--- urban-twins/src/components/MapComponent.js
+++ urban-twins/src/components/MapComponent.js
@@ -23,55 +23,54 @@ function MapComponent() {
           },
         });
 
+        //const neighborhoodsToDisplay = ['upper-west-side.txt'];
         const neighborhoodsToDisplay = ['east-village.txt', 'upper-west-side.txt'];
 
         neighborhoodsToDisplay.forEach(neighborhoodFile => {
 
-        fetch('./neighborhoods/' + neighborhoodFile)
-          .then(response => response.text())
-          .then(data => {
-            let manhattanCoords;
-            try {
+          fetch('./neighborhoods/' + neighborhoodFile)
+            .then(response => response.text())
+            .then(data => {
+              
+              // Parse the polygon data --> Break out into separate function
+              //let manhattanCoords;
+              
               const coordsArray = data.trim().split('\n').map(line => {
-              const [lng, lat] = line.trim().split(/\s+/);
-              return { lat: parseFloat(lat), lng: parseFloat(lng) };
+                const [lng, lat] = line.trim().split(/\s+/);
+                return { lat: parseFloat(lat), lng: parseFloat(lng) };
               });
-              manhattanCoords = coordsArray;
-            } catch (error) {
-              console.error('Error parsing coordinates:', error);
-              return;
-            }
-            
-            // Construct the polygon.
-            const manhattanPolygon = new window.google.maps.Polygon({
-              paths: manhattanCoords,
-              strokeColor: "#FF0000",
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-              fillColor: "#FF0000",
-              fillOpacity: 0.35,
+
+              const manhattanCoords = coordsArray;
+              
+              // Construct the polygon.
+              const manhattanPolygon = new window.google.maps.Polygon({
+                paths: manhattanCoords,
+                strokeColor: "#FF0000",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: "#FF0000",
+                fillOpacity: 0.35,
+              });
+
+              manhattanPolygon.setMap(map);
+
+
+            // Set the label of the polygon
+            const bounds = new window.google.maps.LatLngBounds();
+            manhattanCoords.forEach(coord => bounds.extend(coord));
+            const center = bounds.getCenter();
+            const mapLabel = new window.MapLabel({
+              text: "East Village (Marais)",
+              position: center,
+              map: map,
+              fontSize: 12,
+              align: 'center',
             });
-
-            manhattanPolygon.setMap(map);
-
-          // Calculate the center of the polygon
-          const bounds = new window.google.maps.LatLngBounds();
-          manhattanCoords.forEach(coord => bounds.extend(coord));
-          const center = bounds.getCenter();
-
-          // Create a MapLabel at the center
-          const mapLabel = new window.MapLabel({
-            text: "East Village (Marais)",
-            position: center,
-            map: map,
-            fontSize: 12,
-            align: 'center',
-          });
 
           mapLabel.set('position', center);
         })
+      });
     };
-
     
     initMap();
 
